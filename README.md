f6ff34839841bf1d5a11109c5f1c60266dd4a9a5
Okanon <flat.cydia37jins.pc@gmail.com>
2017-09-15 17:56:51 +0900

    first commit!!!

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# JsonParseViewer
-It is software for shaping and outputting or saving JSON.
+<h1 align="center">
+	<img width="350" src="https://raw.githubusercontent.com/okanon/JsonParseViewer/master/source/JsonParseViewer/Resources/icons/gitIcon.png" alt="Awesome">
+	<br>
+	<br>
+</h1>
+
+<br>
+<br>
+
+<p align="center">
+	<b>It is software for shaping and outputting or saving JSON.</b>
+</p>
+
+<br>
+
+---
+<br>
+
+<p align="center">
+	<sub>Follow me on <a href="https://twitter.com/@xlyr_cs">Twitter</a>.</sub>
+</p>
+
+## How To Use
+Please build it with Visual Studio or [download]() the Windows binary and use it. There is a possibility that it does not operate normally under Windows 7 or below. It requires [.NET Framework 4.6.1](https://www.microsoft.com/en-Us/download/details.aspx?id=49981) to run.
+
+
+Since binary files are integrated with dynamic libraries, binary files can be executed by themselves.
+
+## License
+[![MIT](https://upload.wikimedia.org/wikipedia/commons/f/f8/License_icon-mit-88x31-2.svg)](https://opensource.org/licenses/MIT)
+
+
+To the extent possible under law, [Okanon](https://twitter.com/@xlyr_cs) has waived all copyright and related or neighboring rights to this work.
---
+++ source/JsonParseViewer/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+
+namespace JsonParseViewer
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    partial class MainWindow
+    {
+        string message;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            AppDomain.CurrentDomain.FirstChanceException += First_Change_Exception;
+        }
+
+        private void First_Change_Exception(object sender, System.Runtime.ExceptionServices.FirstChanceExceptionEventArgs e)
+        {
+            string eMember = e.Exception.TargetSite.Name;
+            string eMessage = e.Exception.Message;
+            message = string.Format(
+                "例外が{0}で発生。プログラムは継続します。\n{1}",
+                eMember,
+                eMessage);
+            /*
+            MessageBox.Show(
+                message,
+                "FirstChanceException",
+                MessageBoxButton.OK,
+                MessageBoxImage.Error);
+            */
+        }
+
+        private void Parse(object sender, RoutedEventArgs e)
+        {
+            var target = this.TextBox.Text;
+
+            if (!string.IsNullOrEmpty(target))
+            {
+                try
+                {
+                    this.TextBox.Text = format_json(target);
+                }
+                catch
+                {
+                    this.TextBox.Text = message;
+                }
+            }
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Json Files(*.json)|*.json|Text Files(*.txt)|*.txt|すべてのファイル(*.*)| *.*";
+
+            if (sfd.ShowDialog() == true)
+            {
+                File.WriteAllText(sfd.FileName, this.TextBox.Text);
+            }
+        }
+
+        private void Reset(object sender, RoutedEventArgs e)
+        {
+            this.TextBox.Text = null;
+        }
+
+        private static string format_json(string json)
+        {
+            //dynamic parsedJson = JsonConvert.DeserializeObject(json);
+            return JsonConvert.SerializeObject(JsonConvert.DeserializeObject(json), Formatting.Indented);
+        }
+    }
+}
